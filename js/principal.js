d13cbbf383892fd6a5cf0cccafc583bcc4f05d09
Rafael Ferreira <rafael.ferreira@before.com.br>
2018-07-15 22:02:59 -0400

    Capitulo 2 ok

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,36 @@
 var titulo = document.querySelector(".titulo");
-			console.log(titulo);
-			console.log(titulo.textContent);
+titulo.textContent = "Aparecida Nutricionista";
 
-			titulo.textContent = "Beleza?";
+var paciente = document.querySelector('#primeiro-paciente');
+
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+//var alturaDireto = ((paciente.querySelector('.info-altura')).textContent);
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector('.info-imc');
+
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso < 0 || peso >= 200){
+    console.log("peso inválido");
+    var pesoValido = false;
+    tdImc.textContent = "Peso Inválido!";
+}
+
+if(altura < 0 || altura >= 3.00){
+    console.log("altura inválida");
+    var alturaValida = false;
+    tdImc.textContent = "Altura Inválida!";
+}
+if(pesoValido && alturaValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
+
+
