d4e5cefcbb80100ce9b610eada52a5f9a38dc61c
Rafael Ferreira <rafael.ferreira@before.com.br>
2018-07-15 23:07:01 -0400

    Capitulo 3 OK

--- js/principal.js
+++ js/principal.js
@@ -1,36 +1,45 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector('#primeiro-paciente');
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdPeso = paciente.querySelector('.info-peso');
-var peso = tdPeso.textContent;
 
-//var alturaDireto = ((paciente.querySelector('.info-altura')).textContent);
-var tdAltura = paciente.querySelector('.info-altura');
-var altura = tdAltura.textContent;
+for(var i = 0; i < pacientes.length; i++){
 
-var tdImc = paciente.querySelector('.info-imc');
+    paciente = pacientes[i];
 
-var pesoValido = true;
-var alturaValida = true;
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
 
-if(peso < 0 || peso >= 200){
-    console.log("peso inválido");
-    var pesoValido = false;
-    tdImc.textContent = "Peso Inválido!";
+    //var alturaDireto = ((paciente.querySelector('.info-altura')).textContent);
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector('.info-imc');
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if(peso < 0 || peso >= 200){
+        console.log("peso inválido");
+        var pesoValido = false;
+        tdImc.textContent = "Peso Inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura < 0 || altura >= 3.00){
+        console.log("altura inválida");
+        var alturaValida = false;
+        tdImc.textContent = "Altura Inválida!";
+        paciente.classList.add("paciente-invalido");
+        //paciente.style.backgroundColor = "orange";
+    }
+    if(pesoValido && alturaValida){
+        var imc = peso / (altura * altura);
+         tdImc.textContent = imc.toFixed(2);
+         
+    }
+
 }
-
-if(altura < 0 || altura >= 3.00){
-    console.log("altura inválida");
-    var alturaValida = false;
-    tdImc.textContent = "Altura Inválida!";
-}
-if(pesoValido && alturaValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
-
-
 
 
