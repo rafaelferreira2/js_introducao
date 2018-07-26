39b0ae74a1a1b5ba8e2f3f99da71fac876360ac9
Rafael Ferreira <rafael.ferreira@before.com.br>
2018-07-26 00:22:24 -0400

    metade capitulo 5

---
+++ js/calcula-ims.js
@@ -0,0 +1,48 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll('.paciente');
+
+
+for(var i = 0; i < pacientes.length; i++){
+
+    paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector('.info-imc');
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if(peso < 0 || peso >= 200){
+        console.log("peso inválido");
+        var pesoValido = false;
+        tdImc.textContent = "Peso Inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura < 0 || altura >= 3.00){
+        console.log("altura inválida");
+        var alturaValida = false;
+        tdImc.textContent = "Altura Inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
+    if(pesoValido && alturaValida){
+        var imc = calcumaImc(peso, altura);
+         tdImc.textContent = imc;
+         
+    }
+
+}
+
+function calcumaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
---
+++ js/form.js
@@ -0,0 +1,46 @@
+var botaoAdiciona = document.querySelector("#adicionar-paciente");
+botaoAdiciona.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+   var paciente = obtemPacienteDoFormulario(form);
+   
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calcumaImc(peso, altura);
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
+
+function obtemPacientesForm(form){
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calcumaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+
+}
