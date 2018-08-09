2b6d1ebc386f65594ced4082f551bca79e63078e
Rafael Ferreira <rafael.ferreira@before.com.br>
2018-08-08 21:21:27 -0400

    Finalizado

--- js/filtra.js
+++ js/filtra.js
@@ -1,6 +1,6 @@
 
 var campoFiltro = document.querySelector("#filtrar-tabela");
-
+ 
 campoFiltro.addEventListener("input", function(){
     console.log(this.value);
     var pacientes = document.querySelectorAll(".paciente");
